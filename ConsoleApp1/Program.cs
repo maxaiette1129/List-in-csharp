7ca2096810e7a65f574d5166c5270cb8364f7ba9
maxaiette1129 <maxaiettec@gmail.com>
2023-04-26 22:47:19 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<string> list = new List<string>();
+
+            list.Add("Maxaiette");
+            list.Add("Cubero");
+            list.Add("maxaiettec@gmail.com");
+            string listinside = string.Format("{0},{1},{2}", list[0], list[1], list[2]);
+            Console.WriteLine(listinside);
+
+            
+        }
+    }
+}
